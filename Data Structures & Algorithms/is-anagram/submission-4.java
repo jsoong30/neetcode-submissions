c8c4df99a00261cc34cd7dc87f99da962cade1ba
Jackson J Soong <jacksonjsoong@gmail.com>
2026-08-11 23:21:00 -0400

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sarr = s.toCharArray();
+        char[] tarr = t.toCharArray();
+        
+        Arrays.sort(sarr);
+        Arrays.sort(tarr);
+        String snew = new String(sarr);
+        String tnew = new String(tarr);
+        return tnew.equals(snew);
+    }
+}
